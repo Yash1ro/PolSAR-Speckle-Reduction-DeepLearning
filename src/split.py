38167cff593af4a0bf06ff602f2a7c2dd6949f3b
Yash1ro <LewisGuyh@outlook.com>
2023-04-19 16:40:05 +0800

    分割数据集

--- src/split.py
+++ src/split.py
@@ -8,12 +8,12 @@ def get_imlist(path):
 
 
 def getData(src_path):
-    dest_dir = '../data/val'  # 这个文件夹需要提前建好
+    dest_dir = '../data/train_Pauli'  # 这个文件夹需要提前建好
     img_list = get_imlist(src_path)
     random.shuffle(img_list)
-    le = int(len(img_list) * 0.1)  # 这个可以修改划分比例
+    le = int(len(img_list) * 0.8)  # 这个可以修改划分比例
     for f in img_list[le:]:
         shutil.move(f, dest_dir)
 
 
-getData('../data/val2017/val2017')
+getData('../data/Pauli_data')
