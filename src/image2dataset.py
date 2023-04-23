2a5472560e432dfc69c70dab092ef58a12f44205
Yash1ro <LewisGuyh@outlook.com>
2023-04-23 15:31:28 +0800

    减少了文字输出，增加了进度条

--- src/image2dataset.py
+++ src/image2dataset.py
@@ -2,6 +2,7 @@ import os
 from torchvision import transforms
 from PIL import Image
 import myLogs as m
+from tqdm import tqdm
 
 if not (os.path.exists("../data/train_Pauli")):
     os.mkdir("../data/train_Pauli")
@@ -28,26 +29,23 @@ trans_totensor = transforms.ToTensor()
 trans_resize = transforms.Resize((1200, 800))
 trans_rancrop = transforms.RandomCrop(400)
 
+
 # 创建数据集
 if len(os.listdir("../data/Pauli_data")) == 0:
-    for i in img_list:
+    for i in tqdm(img_list):
         print(i)
         img = Image.open(os.path.join(root_path, target_path, i))
         print(img)
         img_resized = trans_resize(img)
         print(img_resized)
         img_totensor = trans_totensor(img_resized)
-        print(img_totensor)
 
-        for k in range(1500):
+        for k in tqdm(range(1500)):
             img_crop = trans_rancrop(img_totensor)
             toPIL = transforms.ToPILImage()  # 这个函数可以将张量转为PIL图片，由小数转为0-255之间的像素值
             img_dataset = toPIL(img_crop)
-            img_dataset.save("../data/Pauli_data/{}_{}.jpg".format(i, k))
+            img_dataset.save("../data/Pauli_data/{}_{}.jpg".format(i.split("jpg")[0], k))
             writer.add_image("dataset-{}".format(k), img_crop, k)
-            print("写入数据集，类型{}，序号：{}".format(i, k))
-
-
 
 
 
